2812ded57cfa1f413df6915f51357a9ae56c6e9e
Florin Andone <florinandone@users.noreply.github.com>
2023-09-26 15:50:22 +1000

    fix path

--- insert_files.sh
+++ insert_files.sh
@@ -11,6 +11,9 @@ main_file="$1"
 delimiter="$2"
 shift 2  # Remove the first two arguments from the list
 
+# Get the directory of the main file
+main_dir=$(dirname "$main_file")
+
 # Check if the main file exists
 if [ ! -f "$main_file" ]; then
     echo "Error: Main file '$main_file' does not exist."
@@ -19,20 +22,23 @@ fi
 
 # Iterate through the list of files and insert their content after the delimiter
 for file in "$@"; do
-    if [ ! -f "$file" ]; then
-        echo "Warning: File '$file' does not exist. Skipping."
+    # Construct the full path for the extra files
+    extra_file="${main_dir}/${file}"
+    
+    if [ ! -f "$extra_file" ]; then
+        echo "Warning: File '$extra_file' does not exist. Skipping."
     else
         # Use awk to insert the content of the file after the delimiter in the main file
         awk -v delim="$delimiter" '
             BEGIN { found = 0 }
             $0 == delim { found = 1; print; next }
             found { print; }
-        ' "$file" "$main_file" > temp_file
+        ' "$extra_file" "$main_file" > temp_file
 
         # Replace the original main file with the modified content
         mv temp_file "$main_file"
 
-        echo "Content from '$file' inserted after the delimiter in '$main_file'."
+        echo "Content from '$extra_file' inserted after the delimiter in '$main_file'."
     fi
 done
 
