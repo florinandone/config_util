c645f4d759392fe84838477b08dd04d4a594f588
Florin Andone <florinandone@users.noreply.github.com>
2023-09-26 12:15:52 +1000

    add search and save

---
+++ search_and_save.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <filename> <word>"
+    exit 1
+fi
+
+filename="$1"
+word="$2"
+
+if [ ! -f "$filename" ]; then
+    echo "The file '$filename' does not exist."
+    exit 1
+fi
+
+# Extract the file extension
+file_extension="${filename##*.}"
+
+# Create the output filename using the word and the original extension
+output_filename="${word}.${file_extension}"
+
+# Use grep to search for the word in the file and redirect the output to the output file
+grep "$word" "$filename" > "$output_filename"
+
+if [ $? -eq 0 ]; then
+    echo "Content containing '$word' saved to '$output_filename'."
+else
+    echo "The word '$word' was not found in the file '$filename'."
+fi
