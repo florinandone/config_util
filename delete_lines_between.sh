2e34bb64cf0fc080f9979d972121dd8a30dbb44d
Florin Andone <florinandone@users.noreply.github.com>
2023-09-26 22:46:38 +1000

    use tail and head

--- delete_lines_between.sh
+++ delete_lines_between.sh
@@ -1,39 +1,38 @@
 #!/bin/bash
 
-# Check if all required arguments are provided
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 input_file start_delimiter end_delimiter"
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 <input_file> <start_delimiter> <end_delimiter>"
     exit 1
 fi
 
-# Assign command-line arguments to variables
-input_file="$1"
-start_delimiter="$2"
-end_delimiter="$3"
+input_file="$1"       # The file from which lines should be removed
+start_delimiter="$2"  # The starting delimiter
+end_delimiter="$3"    # The ending delimiter
 
 # Check if the input file exists
-if [ ! -f "$input_file" ]; then
-    echo "Error: Input file '$input_file' does not exist."
+if [ ! -e "$input_file" ]; then
+    echo "Input file '$input_file' does not exist."
     exit 1
 fi
 
-# Create a temporary file
-temp_file=$(mktemp)
+# Create a temporary file for the updated content
+temp_file="temp_output.txt"
 
-# Use awk to remove lines containing spaces between the delimiters
-awk -v start="$start_delimiter" -v end="$end_delimiter" '
-  $0 ~ start { in_block=1; print; next }
-  in_block {
-    if ($0 ~ end) {
-      in_block=0
-      print
-    }
-  }
-  !in_block
-' "$input_file" > "$temp_file"
+# Use grep to find the line numbers of the start and end delimiters
+start_line=$(grep -n "$start_delimiter" "$input_file" | cut -d ":" -f 1)
+end_line=$(grep -n "$end_delimiter" "$input_file" | cut -d ":" -f 1)
 
-# Replace the original file with the modified content
+# Check if the delimiters were found
+if [ -z "$start_line" ] || [ -z "$end_line" ]; then
+    echo "Start or end delimiter not found in the input file."
+    exit 1
+fi
+
+# Use head and tail to extract lines before and after the specified range
+head -n "$((start_line - 1))" "$input_file" > "$temp_file"
+tail -n "+$((end_line + 1))" "$input_file" >> "$temp_file"
+
+# Replace the input file with the temporary file
 mv "$temp_file" "$input_file"
 
-# Output confirmation message
-echo "Lines between '$start_delimiter' and '$end_delimiter' containing spaces deleted in $input_file"
+echo "Lines between '$start_delimiter' and '$end_delimiter' deleted successfully."
