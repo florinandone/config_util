e6b730f4c0bd58e61a9a2e5d414f6a5730530f3c
Florin Andone <florinandone@users.noreply.github.com>
2023-09-26 15:15:23 +1000

    make it simpler

--- replace_word.sh
+++ replace_word.sh
@@ -17,15 +17,8 @@ if [ ! -f "$input_file" ]; then
     exit 1
 fi
 
-# Extract the directory and extension from the input file
-directory=$(dirname "$input_file")
-extension="${input_file##*.}"
-
-# Create the output filename using the new word and the same extension
-output_file="${directory}/${new_word}.${extension}"
-
-# Use grep and cat to replace the word and save it to the output file
-cat "$input_file" | grep -o '\w*\|[^[:space:]]*' | sed "s/$old_word/$new_word/g" > "$output_file"
+# Use sed to replace the word in the input file and save it in place
+sed -i "s/$old_word/$new_word/g" "$input_file"
 
 # Output confirmation message
-echo "Word '$old_word' replaced with '$new_word' in $input_file and saved as $output_file"
+echo "Word '$old_word' replaced with '$new_word' in $input_file"
