7c831a0ea0d3330dede25da0a0129624cebfc648
Florin Andone <florinandone@users.noreply.github.com>
2023-09-26 15:47:29 +1000

    for insert files insert after and do not delete separator

--- insert_files.sh
+++ insert_files.sh
@@ -17,63 +17,23 @@ if [ ! -f "$main_file" ]; then
     exit 1
 fi
 
-# Get the directory of the main file
-main_dir=$(dirname "$main_file")
-
-# Create a temporary file
-temp_file=$(mktemp)
-
-# Prepare a list of files to insert
-inserted_files=()
-
-# Iterate through the list of extra files
-for extra_file in "$@"; do
-    # Construct the full path for the extra file
-    full_extra_file="${main_dir}/${extra_file}"
-    
-    if [ -f "$full_extra_file" ]; then
-        inserted_files+=("$full_extra_file")
+# Iterate through the list of files and insert their content after the delimiter
+for file in "$@"; do
+    if [ ! -f "$file" ]; then
+        echo "Warning: File '$file' does not exist. Skipping."
     else
-        echo "Warning: File '$full_extra_file' does not exist. Skipping."
+        # Use awk to insert the content of the file after the delimiter in the main file
+        awk -v delim="$delimiter" '
+            BEGIN { found = 0 }
+            $0 == delim { found = 1; print; next }
+            found { print; }
+        ' "$file" "$main_file" > temp_file
+
+        # Replace the original main file with the modified content
+        mv temp_file "$main_file"
+
+        echo "Content from '$file' inserted after the delimiter in '$main_file'."
     fi
 done
-
-# Use awk to process the main file and insert the content of the extra files after the delimiter
-awk -v delim="$delimiter" -v files_to_insert="${inserted_files[*]}" '
-    BEGIN { inside_block = 0; file_index = 1 }
-    
-    # Split the files_to_insert variable into an array
-    function split_files(array, str) {
-        n = split(str, a)
-        for (i = 1; i <= n; i++) {
-            array[i] = a[i]
-        }
-        return n
-    }
-
-    # Process each line of the input file
-    {
-        # Check if the line is the delimiter
-        if ($0 == delim) {
-            inside_block = 1
-            next
-        }
-
-        # If inside the delimiter block, insert the content of the next file
-        if (inside_block == 1 && file_index <= split_files(files_array, files_to_insert)) {
-            while ((getline line < files_array[file_index]) > 0) {
-                print line
-            }
-            close(files_array[file_index])
-            file_index++
-        }
-        
-        # Print the current line
-        print $0
-    }
-' "$main_file" > "$temp_file"
-
-# Replace the original main file with the modified content
-mv "$temp_file" "$main_file"
 
 echo "Insertion complete."
