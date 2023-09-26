cc2e11acb0377793cc2eb1e27a5ac74003cb2998
Florin Andone <florinandone@users.noreply.github.com>
2023-09-26 18:49:52 +1000

    insert_file_after

--- insert_file_after.sh
+++ insert_file_after.sh
@@ -22,16 +22,17 @@ fi
 # Create a temporary file
 temp_file=$(mktemp)
 
-# Check if the delimiter exists in the main file
-if grep -q "$delimiter" "$main_file"; then
-    # Split the main file into two parts using the delimiter
-    awk -v d="$delimiter" 'BEGIN {RS=d; ORS=""} NR == 1 {print; exit}' "$main_file" > "$temp_file"
+# Find the line number of the delimiter in the main file
+delimiter_line=$(grep -n "$delimiter" "$main_file" | cut -d: -f1)
+
+if [ -n "$delimiter_line" ]; then
+    # Insert the content of the file to insert after the delimiter line
+    head -n "$delimiter_line" "$main_file" > "$temp_file"
     cat "$file_to_insert" >> "$temp_file"
-    awk -v d="$delimiter" 'BEGIN {RS=d; ORS=""} NR > 1 {print d $0}' "$main_file" >> "$temp_file"
+    tail -n +"$((delimiter_line + 1))" "$main_file" >> "$temp_file"
 else
-    # If the delimiter doesn't exist, simply append the content of the file to insert
+    # If the delimiter doesn't exist, append the content of the file to insert
     cat "$main_file" > "$temp_file"
-    echo "$delimiter" >> "$temp_file"
     cat "$file_to_insert" >> "$temp_file"
 fi
 
